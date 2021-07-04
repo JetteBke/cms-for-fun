f75aacffa179c0e0490f1219e8577aa9064b2f54
Jette Bakemeier <jette.bakemeier@thoughtworks.com>
2021-07-04 12:45:49 +0200

    JB: Redirect to detail page after editing contact

--- src/Contacts/EditContact/EditContact.tsx
+++ src/Contacts/EditContact/EditContact.tsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {ContactForm} from "../ContactForm/ContactForm";
 import {getContact, updateContact} from "../ContactService";
 import {Contact} from "../Contact";
-import {useParams} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 
 interface Params {
     contactId: string
@@ -11,19 +11,27 @@ interface Params {
 export const EditContact: React.FC = () => {
 
     const {contactId} = useParams<Params>()
+    const history = useHistory()
     const [contact, setContact] = useState<Contact>()
 
-    React.useEffect( () => {
+    React.useEffect(() => {
         getContact(parseInt(contactId)).then(resp => setContact(resp))
-    },[])
+    }, [])
+
+    const updateContactAndRedirect = async (contact: Contact) => {
+        await updateContact(contact)
+        history.push({
+            pathname: `/view/${contactId}`,
+        })
+    }
 
     return (
         <>
             <div className='container'>
                 <h1 className='title'>Kontakt bearbeiten</h1>
                 {contact ?
-                <ContactForm contact={contact} onSave={updateContact}/>
-                : null}
+                    <ContactForm contact={contact} onSave={updateContactAndRedirect}/>
+                    : null}
             </div>
         </>
     )
