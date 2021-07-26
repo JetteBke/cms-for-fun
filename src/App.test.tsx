0b22b111cc041dd3c48c55ae4c5eef69ae2acbc0
Jette Bakemeier <jette.bakemeier@thoughtworks.com>
2021-07-26 20:31:41 +0200

    JB: Remove ad link and add edit icon svg

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 test('renders different routes of the app', () => {
   const wrapper = render(<App />);
 
-  expect(wrapper.getAllByRole("link")).toHaveLength(5);
+  expect(wrapper.getAllByRole("link")).toHaveLength(3);
   expect(wrapper.getByText("Alle Kontakte")).toBeInTheDocument()
   expect(wrapper.getByText("Kontakt hinzufügen")).toBeInTheDocument()
   expect(wrapper.getByText("Datei hochladen")).toBeInTheDocument()
