427af8b6f9804814f74eda55a9daf675dfd0d608
Jette Bakemeier <jette.bakemeier@thoughtworks.com>
2021-07-26 19:32:23 +0200

    JB: Redirect after a contact is saved

--- src/Contacts/AddContact/AddContact.tsx
+++ src/Contacts/AddContact/AddContact.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import {ContactForm} from "../ContactForm/ContactForm";
 import {saveContact} from "../ContactService";
+import {Contact} from "../Contact";
+import {useHistory} from "react-router-dom";
 
 export const AddContact: React.FC = () => {
+
+    const history = useHistory()
+    const saveContactAndRedirect = async (contact: Contact) => {
+        const successfulSave = await saveContact(contact)
+        if (successfulSave) {
+            history.push({
+                pathname: '/list',
+            })
+        }
+    }
 
     return (
         <>
             <div className='container'>
                 <h1 className='title'>Kontakt hinzufügen</h1>
-                <ContactForm onSave={saveContact}/>
+                <ContactForm onSave={saveContactAndRedirect}/>
             </div>
         </>
     )
